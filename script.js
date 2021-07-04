86cae930595949117028006b86b71d7831bc0ccb
Arun <arunnair018@gmail.com>
2021-07-04 07:29:38 +0530

    added error handling

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const formFile = document.querySelector("#formFileLg");
 const displayHtml = document.querySelector("#displayText");
 const checkButton = document.querySelector("#checkButton");
-const errorHtml = document.querySelector(".error");
-console.log;
+const errorSelector = document.querySelector(".error");
+
+// read the file and show content
 formFile.addEventListener("change", function () {
   checkButton.style.visibility = "visible";
   let file = formFile.files[0];
-  console.log(file);
   let reader = new FileReader();
   reader.addEventListener("load", function (e) {
     let text = e.target.result;
     displayHtml.innerHTML = text;
   });
   reader.readAsText(file);
-  formFile.value = "";
+  // reset the file form value to null
+  errorSelector.style.display = "none";
 });
 
+// hit the API and mark bad words with suggestions
 checkButton.addEventListener("click", function (event) {
   event.preventDefault();
-  checkButton.style.visibility = "hidden";
+  errorSelector.style.display = "none";
+  if (formFile && formFile.files.length === 0) {
+    errorSelector.style.display = "block";
+    return;
+  }
+  checkButton.style.display = "none";
+  formFile.value = null;
   let lines = displayHtml.innerHTML.split("\n");
+  // hitting API for each line rather the whole text
   lines.forEach(async (line, index) => {
     return fetch(
       `https://api.textgears.com/spelling?text=${line}&language=en-GB&whitelist=&dictionary_id=&key=z88J6TXRGQ8HDrxp`
@@ -28,12 +37,16 @@ checkButton.addEventListener("click", function (event) {
         return res.json();
       })
       .then((data) => {
+        // process each error
         data.response.errors.forEach((error) => {
-          console.log(error.bad);
+          // create suggestion list
           let suggestion = "";
           error.better.forEach((element, index) => {
             suggestion += `<li class="my-tooltip-options ${error.bad}-${index}" onclick="handleChange()">${element}</li>`;
           });
+
+          // create new html and replace the word with highlighting and suggestion
+          // to new html, add eventlistner to be invoked when clciked on suggestion
           let newHtml = `<div class="my-tooltip">${error.bad}<span class="my-tooltiptext"><ul>${suggestion}</ul></span></div>`;
           displayHtml.innerHTML = displayHtml.innerHTML.replace(
             error.bad,
@@ -44,7 +57,10 @@ checkButton.addEventListener("click", function (event) {
   });
 });
 
+// function replace the bad word with suggestion
+// invoked with suggestion event handler
 const handleChange = function () {
+  // get parent and replace it with suggestion
   let parent = this.event.target.closest(".my-tooltip");
   parent.replaceWith(this.event.target.innerHTML);
 };
